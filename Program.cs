ae969e0868ac57780f429dd7810dc69df9210ea4
Ciibelle <cibellegon6565@gmail.com>
2021-09-27 19:18:54 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RevisaoForEforeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] alimentos = new string[6];
+
+           alimentos[0] = "Arroz";
+           alimentos[1] = "Feijão";
+           alimentos[2] = "Macarrão";
+           alimentos[3] = "Leite";
+           alimentos[4] = "Açucar";
+           alimentos[5] = "Café";
+
+           Console.WriteLine($"*Lista De Alimentos*");
+           Console.WriteLine(" ");
+           Console.WriteLine($"LAÇO FOREACH");
+            
+           foreach(string comida in alimentos)
+           {
+               Console.WriteLine(comida);
+           }
+
+           Console.WriteLine("FINAL DO LAÇO FOREACH");
+           Console.WriteLine(" ");
+           Console.WriteLine("LAÇO FOR");
+
+           for(int i = 0; i< alimentos.Length; i++)
+           {
+               Console.WriteLine(alimentos[i]);
+           }
+
+           Console.WriteLine("FINAL DO LAÇO FOR");
+
+        }
+    }
+}
